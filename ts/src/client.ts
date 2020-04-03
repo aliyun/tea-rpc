aaea46c5ec4daaff79867d59364fe1bbc52539c4
yuanyaoqi <yuanyaoqi@live.com>
2020-04-03 18:08:25 +0800

    support ts

---
+++ ts/src/client.ts
@@ -0,0 +1,279 @@
+// This file is auto-generated, don't edit it
+import Util, * as $Util from '@alicloud/tea-util';
+import Credential, * as $Credential from '@alicloud/credentials';
+import RPCUtil from '@alicloud/rpc-util';
+import { Readable } from 'stream';
+import * as $tea from '@alicloud/tea-typescript';
+
+export class Config extends $tea.Model {
+  accessKeyId?: string;
+  accessKeySecret?: string;
+  network?: string;
+  suffix?: string;
+  type?: string;
+  securityToken?: string;
+  endpoint?: string;
+  protocol?: string;
+  regionId?: string;
+  productId?: string;
+  userAgent?: string;
+  readTimeout?: number;
+  connectTimeout?: number;
+  httpProxy?: string;
+  httpsProxy?: string;
+  noProxy?: string;
+  socks5Proxy?: string;
+  socks5NetWork?: string;
+  maxIdleConns?: number;
+  endpointType?: string;
+  openPlatformEndpoint?: string;
+  static names(): { [key: string]: string } {
+    return {
+      accessKeyId: 'accessKeyId',
+      accessKeySecret: 'accessKeySecret',
+      network: 'network',
+      suffix: 'suffix',
+      type: 'type',
+      securityToken: 'securityToken',
+      endpoint: 'endpoint',
+      protocol: 'protocol',
+      regionId: 'regionId',
+      productId: 'productId',
+      userAgent: 'userAgent',
+      readTimeout: 'readTimeout',
+      connectTimeout: 'connectTimeout',
+      httpProxy: 'httpProxy',
+      httpsProxy: 'httpsProxy',
+      noProxy: 'noProxy',
+      socks5Proxy: 'socks5Proxy',
+      socks5NetWork: 'socks5NetWork',
+      maxIdleConns: 'maxIdleConns',
+      endpointType: 'endpointType',
+      openPlatformEndpoint: 'openPlatformEndpoint',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      accessKeyId: 'string',
+      accessKeySecret: 'string',
+      network: 'string',
+      suffix: 'string',
+      type: 'string',
+      securityToken: 'string',
+      endpoint: 'string',
+      protocol: 'string',
+      regionId: 'string',
+      productId: 'string',
+      userAgent: 'string',
+      readTimeout: 'number',
+      connectTimeout: 'number',
+      httpProxy: 'string',
+      httpsProxy: 'string',
+      noProxy: 'string',
+      socks5Proxy: 'string',
+      socks5NetWork: 'string',
+      maxIdleConns: 'number',
+      endpointType: 'string',
+      openPlatformEndpoint: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+
+export default class Client {
+  _endpoint: string;
+  _regionId: string;
+  _protocol: string;
+  _userAgent: string;
+  _endpointRule: string;
+  _endpointMap: {[key: string ]: string};
+  _suffix: string;
+  _readTimeout: number;
+  _connectTimeout: number;
+  _httpProxy: string;
+  _httpsProxy: string;
+  _socks5Proxy: string;
+  _socks5NetWork: string;
+  _noProxy: string;
+  _network: string;
+  _productId: string;
+  _maxIdleConns: number;
+  _endpointType: string;
+  _openPlatformEndpoint: string;
+  _credential: Credential;
+
+  constructor(config: Config) {
+    let credentialConfig : $Credential.Config = null;
+    if (Util.isUnset($tea.toMap(config))) {
+      config = new Config({ });
+      this._credential = new Credential(null);
+    } else {
+      credentialConfig = new $Credential.Config({
+        accessKeyId: config.accessKeyId,
+        type: config.type,
+        accessKeySecret: config.accessKeySecret,
+        securityToken: config.securityToken,
+      });
+      this._credential = new Credential(credentialConfig);
+    }
+
+    this._network = config.network;
+    this._suffix = config.suffix;
+    this._endpoint = config.endpoint;
+    this._protocol = config.protocol;
+    this._regionId = config.regionId;
+    this._userAgent = config.userAgent;
+    this._readTimeout = config.readTimeout;
+    this._connectTimeout = config.connectTimeout;
+    this._httpProxy = config.httpProxy;
+    this._httpsProxy = config.httpsProxy;
+    this._noProxy = config.noProxy;
+    this._socks5Proxy = config.socks5Proxy;
+    this._socks5NetWork = config.socks5NetWork;
+    this._maxIdleConns = config.maxIdleConns;
+    this._endpointType = config.endpointType;
+    this._openPlatformEndpoint = config.openPlatformEndpoint;
+  }
+
+  async doRequest(action: string, protocol: string, method: string, authType: string, query: {[key: string]: any}, body: {[key: string]: any}, runtime: $Util.RuntimeOptions): Promise<{[key: string]: any}> {
+    let _runtime: { [key: string]: any } = {
+      timeouted: "retry",
+      readTimeout: Util.defaultNumber(runtime.readTimeout, this._readTimeout),
+      connectTimeout: Util.defaultNumber(runtime.connectTimeout, this._connectTimeout),
+      httpProxy: Util.defaultString(runtime.httpProxy, this._httpProxy),
+      httpsProxy: Util.defaultString(runtime.httpsProxy, this._httpsProxy),
+      noProxy: Util.defaultString(runtime.noProxy, this._noProxy),
+      maxIdleConns: Util.defaultNumber(runtime.maxIdleConns, this._maxIdleConns),
+      retry: {
+        retryable: runtime.autoretry,
+        maxAttempts: Util.defaultNumber(runtime.maxAttempts, 3),
+      },
+      backoff: {
+        policy: Util.defaultString(runtime.backoffPolicy, "no"),
+        period: Util.defaultNumber(runtime.backoffPeriod, 1),
+      },
+      ignoreSSL: runtime.ignoreSSL,
+    }
+
+    let _lastRequest = null;
+    let _now = Date.now();
+    let _retryTimes = 0;
+    while ($tea.allowRetry(_runtime['retry'], _retryTimes, _now)) {
+      if (_retryTimes > 0) {
+        let _backoffTime = $tea.getBackoffTime(_runtime['backoff'], _retryTimes);
+        if (_backoffTime > 0) {
+          await $tea.sleep(_backoffTime);
+        }
+      }
+
+      _retryTimes = _retryTimes + 1;
+      try {
+        let request_ = new $tea.Request();
+        request_.protocol = Util.defaultString(this._protocol, protocol);
+        request_.method = method;
+        request_.pathname = "/";
+        request_.query = RPCUtil.query({
+          Action: action,
+          Format: "json",
+          Timestamp: RPCUtil.getTimestamp(),
+          Version: "2019-12-30",
+          SignatureNonce: Util.getNonce(),
+          ...query,
+        });
+        if (!Util.isUnset(body)) {
+          let tmp = Util.anyifyMapValue(RPCUtil.query(body));
+          request_.body = new $tea.BytesReadable(Util.toFormString(tmp));
+        }
+
+        // endpoint is setted in product client
+        request_.headers = {
+          host: this._endpoint,
+          'user-agent': this.getUserAgent(),
+        };
+        if (!Util.equalString(authType, "Anonymous")) {
+          let accessKeyId = await this.getAccessKeyId();
+          let accessKeySecret = await this.getAccessKeySecret();
+          let securityToken = await this.getSecurityToken();
+          if (!Util.empty(securityToken)) {
+            request_.query["SecurityToken"] = securityToken;
+          }
+
+          request_.query["SignatureMethod"] = "HMAC-SHA1";
+          request_.query["SignatureVersion"] = "1.0";
+          request_.query["AccessKeyId"] = accessKeyId;
+          request_.query["Signature"] = RPCUtil.getSignature(request_, accessKeySecret);
+        }
+
+        _lastRequest = request_;
+        let response_ = await $tea.doAction(request_, _runtime);
+
+        let obj = await Util.readAsJSON(response_.body);
+        let res = Util.assertAsMap(obj);
+        if (Util.is4xx(response_.statusCode) || Util.is5xx(response_.statusCode)) {
+          throw $tea.newError({
+            message: res["Message"],
+            data: res,
+            code: res["Code"],
+          });
+        }
+
+        return res;
+      } catch (ex) {
+        if ($tea.isRetryable(ex)) {
+          continue;
+        }
+        throw ex;
+      }
+    }
+
+    throw $tea.newUnretryableError(_lastRequest);
+  }
+
+  getUserAgent(): string {
+    let userAgent = Util.getUserAgent(this._userAgent);
+    return userAgent;
+  }
+
+  async getAccessKeyId(): Promise<string> {
+    if (Util.isUnset(this._credential)) {
+      return "";
+    }
+
+    let accessKeyId = await this._credential.getAccessKeyId();
+    return accessKeyId;
+  }
+
+  async getAccessKeySecret(): Promise<string> {
+    if (Util.isUnset(this._credential)) {
+      return "";
+    }
+
+    let secret = await this._credential.getAccessKeySecret();
+    return secret;
+  }
+
+  async getSecurityToken(): Promise<string> {
+    if (Util.isUnset(this._credential)) {
+      return "";
+    }
+
+    let token = await this._credential.getSecurityToken();
+    return token;
+  }
+
+  checkConfig(config: Config): void {
+    if (Util.empty(this._endpointRule) && Util.empty(config.endpoint)) {
+      throw $tea.newError({
+        name: "ParameterMissing",
+        message: "'config.endpoint' can not be empty",
+      });
+    }
+
+  }
+
+}
